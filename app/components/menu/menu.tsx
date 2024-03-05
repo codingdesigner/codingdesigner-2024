2a3cdacb1bcff954c8b319b30d4072804772d0fb
Gwen Wendell <gwen@thecodingdesigner.com>
2024-03-04 20:13:00 -0500

    menu drawer styles

--- app/components/menu/menu.tsx
+++ app/components/menu/menu.tsx
@@ -1,10 +1,30 @@
 'use client';
 import React, { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 import styles from './menu.module.css';
 
 export interface menuProps {
   prop?: string;
+}
+
+function MenuList() {
+  return (
+    <ul role="menu" className={styles.menu}>
+      <li className={styles.menuItem}>
+        <a href="/portfolio" className={styles.menuLink}>Portfolio</a>
+      </li>
+      <li className={styles.menuItem}>
+        <a href="/cv" className={styles.menuLink}>CV</a>
+      </li>
+      <li className={styles.menuItem}>
+        <a href="/about" className={styles.menuLink}>About</a>
+      </li>
+      <li className={styles.menuItem}>
+        <a href="/contact" className={styles.menuLink}>Contact</a>
+      </li>
+    </ul>
+  );
 }
 
 export function Menu({ prop = 'default value' }: menuProps) {
@@ -13,28 +33,27 @@ export function Menu({ prop = 'default value' }: menuProps) {
   const onHamburgerClick = () => {
     const newState = (menuActive) ? false : true;
     setMenuActive(newState);
-    console.log(menuActive)
+    console.log(minSmall)
   };
+
+  const minSmall = useMediaQuery({
+    query: `(max-width: 632px)`,
+  });
+
   return (
     <React.Fragment>
-      <div className={styles.menuButton}
-        onClick={onHamburgerClick}>Menu</div>
-      <div className={(menuActive)? styles.drawerOpen: styles.drawerClosed}>
-        <ul role="menu" className={styles.menu}>
-          <li className={styles.menuItem}>
-            <a href="/portfolio" className={styles.menuLink}>Portfolio</a>
-          </li>
-          <li className={styles.menuItem}>
-            <a href="/cv" className={styles.menuLink}>CV</a>
-          </li>
-          <li className={styles.menuItem}>
-            <a href="/about" className={styles.menuLink}>About</a>
-          </li>
-          <li className={styles.menuItem}>
-            <a href="/contact" className={styles.menuLink}>Contact</a>
-          </li>
-        </ul>
-      </div>
+      {minSmall && (
+        <React.Fragment>
+          <div className={styles.menuButton}
+            onClick={onHamburgerClick}>Menu</div>
+          <div className={(menuActive) ? styles.drawerOpen : styles.drawerClosed}>
+            <MenuList />
+          </div>
+        </React.Fragment>
+      )}
+      {!minSmall && (
+        <MenuList />
+      )}
     </React.Fragment>
   );
 }
