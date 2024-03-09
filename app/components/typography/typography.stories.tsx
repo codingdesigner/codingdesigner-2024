1a0709399059558ac904c3230374f5a738b9855b
Gwen Wendell <gwen@thecodingdesigner.com>
2024-03-09 11:50:15 -0500

    type margin adjustments

--- app/components/typography/typography.stories.tsx
+++ app/components/typography/typography.stories.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import type {Meta, StoryObj} from '@storybook/react';
 
 import { Typography } from './typography';
-import { textSection } from './story-content/text'
-import { embeddedSection } from './story-content/embedded'
-import { formsSection } from './story-content/forms'
+import { textSection } from './story-content/text';
+import { embeddedSection } from './story-content/embedded';
+import { formsSection } from './story-content/forms';
 
 const meta: Meta<typeof Typography> = {
   title: 'Typography',
