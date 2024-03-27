d1f0488de4ec3d2c2ba627d59c820f1c11eaafd2
Gwen Wendell <gwen@thecodingdesigner.com>
2024-03-27 00:31:03 -0400

    add figure component 🟢 figure.module.css - New file for styling figure component 🟢 figure.stories.tsx - New file for storybook stories of figure component 🟢 figure.tsx - New file for the figure component implementation 🟢 index.ts - New file for exporting figure component 🟢 figure.test.tsx - New file for testing the figure component

---
+++ app/components/figure/figure.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import type {Meta, StoryObj} from '@storybook/react';
+import brandlessCard from "../../static/cards/card-brandless.png"
+console.log(['brandlessCard', brandlessCard])
+
+import {Figure} from './figure';
+
+const meta: Meta<typeof Figure> = {
+  title: 'Portfolio/Figure',
+  component: Figure,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Figure>;
+
+export const Basic: Story = {
+  args: {
+    imageFile: brandlessCard,
+    altText: "some alt text",
+    caption: "A basic caption"
+  }
+};
