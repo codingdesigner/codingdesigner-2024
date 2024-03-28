1ea49b3366721ce9080ec82f582aece23ba55ddf
Gwen Wendell <gwen@thecodingdesigner.com>
2024-03-28 17:33:40 -0400

    🔴 .storybook/preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,1 @@
-// import type { Preview } from "@storybook/react";
-
 import '../app/global/global.css'
-
-// const preview: Preview = {
-//   parameters: {
-//     actions: { argTypesRegex: '^on.*' },
-//   },
-// };
-
-// export default preview;
