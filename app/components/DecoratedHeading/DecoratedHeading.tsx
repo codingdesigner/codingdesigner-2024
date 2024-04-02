7b5e6e6341defe4dfa2a64fd58b37873150c536f
Gwen Wendell <gwen@thecodingdesigner.com>
2024-04-02 12:46:05 -0400

    🛠️ DecoratedHeading.tsx -> Updated colorVariables type and added ColorVariableProps interface. Modified DecoratedHeadingProps interface to use ColorVariableProps. Refactored DecoratedHeading function to use the updated interfaces and type.

--- app/components/DecoratedHeading/DecoratedHeading.tsx
+++ app/components/DecoratedHeading/DecoratedHeading.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './DecoratedHeading.module.css';
 
-const colorVariables: { [key: string] } = {
+const colorVariables: { [key: string]: string } = {
   'pink': 'var(--color-accent-5)',
   'blue': 'var(--color-accent-1)',
   'green': 'var(--color-accent-2)',
@@ -12,27 +12,29 @@ const colorVariables: { [key: string] } = {
   'white': 'var(--color-grayscale-white)',
   'page-text': 'var(--main-text-color)',
   'page': 'var(--main-bg-color)'
+};
+
+export interface ColorVariableProps {
+  color: 'pink' | 'blue' | 'green' | 'peach' | 'lavender' | 'black' | 'white' | 'page' | 'page-text';
 }
 
 export interface DecoratedHeadingProps {
-  children?;
+  children: React.ReactNode;
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div';
-  background?: 'pink' | 'blue' | 'green' | 'peach' | 'lavender' | 'black' | 'white' | 'page' | 'page-text';
-  text?: 'pink' | 'blue' | 'green' | 'peach' | 'lavender' | 'black' | 'white' | 'page' | 'page-text';
-  shadow?: 'pink' | 'blue' | 'green' | 'peach' | 'lavender' | 'black' | 'white' | 'page' | 'page-text';
+  background?: ColorVariableProps['color'];
+  text?: ColorVariableProps['color'];
+  shadow?: ColorVariableProps['color'];
 }
 
 export function DecoratedHeading({ children, as: Element = 'h1', background = 'pink', text = 'page-text', shadow = 'page' }: DecoratedHeadingProps) {
-  return (
-    <Element
-      className={styles.DecoratedHeading}
-      style={
-        {
+  const style = {
           '--color-background': colorVariables[background],
           '--color-text': colorVariables[text],
           '--color-shadow': colorVariables[shadow]
-        } as React.CSSProperties}
-    >
+        } as React.CSSProperties;
+
+  return (
+    <Element className={styles.DecoratedHeading} style={style}>
       {children}
     </Element>
   );
