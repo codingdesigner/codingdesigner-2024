26a62f265fa004bd5a807a8957f7d21700f3886b
Gwen Wendell <gwen@thecodingdesigner.com>
2024-04-02 12:47:07 -0400

    🛠️ DecoratedHeading.tsx -> Added color variable definitions and props for DecoratedHeading component

--- app/components/DecoratedHeading/DecoratedHeading.tsx
+++ app/components/DecoratedHeading/DecoratedHeading.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import styles from './DecoratedHeading.module.css';
 
+// Define color variables using CSS custom properties
 const colorVariables: { [key: string]: string } = {
   'pink': 'var(--color-accent-5)',
   'blue': 'var(--color-accent-1)',
@@ -14,10 +15,12 @@ const colorVariables: { [key: string]: string } = {
   'page': 'var(--main-bg-color)'
 };
 
+// Define props for color variables
 export interface ColorVariableProps {
   color: 'pink' | 'blue' | 'green' | 'peach' | 'lavender' | 'black' | 'white' | 'page' | 'page-text';
 }
 
+// Define props for DecoratedHeading component
 export interface DecoratedHeadingProps {
   children: React.ReactNode;
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div';
@@ -26,13 +29,16 @@ export interface DecoratedHeadingProps {
   shadow?: ColorVariableProps['color'];
 }
 
+// Define DecoratedHeading component
 export function DecoratedHeading({ children, as: Element = 'h1', background = 'pink', text = 'page-text', shadow = 'page' }: DecoratedHeadingProps) {
+  // Set inline styles using color variables
   const style = {
-          '--color-background': colorVariables[background],
-          '--color-text': colorVariables[text],
-          '--color-shadow': colorVariables[shadow]
-        } as React.CSSProperties;
+    '--color-background': colorVariables[background],
+    '--color-text': colorVariables[text],
+    '--color-shadow': colorVariables[shadow]
+  } as React.CSSProperties;
 
+  // Render DecoratedHeading component with specified styles
   return (
     <Element className={styles.DecoratedHeading} style={style}>
       {children}
