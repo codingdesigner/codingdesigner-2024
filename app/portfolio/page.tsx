ce1b8955ad59abdbd0e139464c6f605244c38e07
Gwen Wendell <gwen@thecodingdesigner.com>
2024-04-30 19:03:23 -0400

    🛠️ app/portfolio/page.tsx -> Added StripesWrapper, restructured content

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { LayoutColumns, Column } from "../components/LayoutColumns";
 import { DecoratedHeading } from "../components/DecoratedHeading";
 import { ParagraphWrapper } from "../components/ParagraphWrapper";
+import { StripesWrapper } from "../components/StripesWrapper";
 
 import { PortfolioIntro } from "./content/PortfolioIntro"
 import { TokenDrivenDesign } from "./content/TokenDrivenDesign";
@@ -20,19 +21,21 @@ const Page = () => {
       <ParagraphWrapper>
         <p>Welcome to my professional portfolio! I'm a seasoned web designer and front-end developer with a specialized focus on developing comprehensive design systems. Here, I've assembled a showcase of projects that reflect my commitment to marrying form with function, creating user-centric designs that drive business success.</p>
       </ParagraphWrapper>
-      <LayoutColumns>
-        <Column>
-          <PortfolioIntro />
-        </Column>
-        <Column>
-          <TokenDrivenDesign />
-        </Column>
-      </LayoutColumns>
-      <CardExamples />
-      <EditorialExpression />
-      <BrandIdentityProcess />
-      <TokenTypes />
-      <Storefront />
+      <StripesWrapper>
+        <LayoutColumns>
+          <Column>
+            <PortfolioIntro />
+          </Column>
+          <Column>
+            <TokenDrivenDesign />
+          </Column>
+        </LayoutColumns>
+        <CardExamples />
+        <EditorialExpression />
+        <BrandIdentityProcess />
+        <TokenTypes />
+        <Storefront />
+      </StripesWrapper>
       <ParagraphWrapper>
         <p>As an expert in web design and front-end development, my goal is to create systems that are not just functional but also adjustable, scalable, and intuitively aligned with each brand’s ethos. I've focused on ensuring that every project I handle is infused with strategic thought, precision, and a keen eye toward future needs, enabling seamless digital experiences that stand the test of time.</p>
 
