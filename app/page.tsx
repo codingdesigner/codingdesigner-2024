b5251b94340a2c186591dc8fa460dc3b563fb515
Gwen Wendell <gwen@thecodingdesigner.com>
2024-05-01 22:14:22 -0400

    🛠️ app/page.tsx -> Updated component structure and imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import React from "react"
+import { DecoratedHeading } from "./components/DecoratedHeading"
+import { Figure } from "./components/figure"
+
+import gwenGoodLight from "./static/gwendy/gwendy-good-light.png"
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <React.Fragment>
+      <DecoratedHeading background="peach">Design Systems for Big Needs</DecoratedHeading>
+      <Figure
+        imageFile={gwenGoodLight}
+        altText="Gwendy Wendell looking at the camera"
+        modalImageFile={null}
+        circleCrop={true}
+        grayScale={true}
+      />
+    </React.Fragment>
+  )
 }
