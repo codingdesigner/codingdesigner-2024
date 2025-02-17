a8f4333e1779e263bda02291057b1196503bb443
Gwen Wendell <gwen@thecodingdesigner.com>
2025-02-17 13:40:45 -0500

    🛠️ RechartsTest.tsx -> Added radar chart components

--- app/components/experiments/components/RechartsTest/RechartsTest.tsx
+++ app/components/experiments/components/RechartsTest/RechartsTest.tsx
@@ -1,58 +1,92 @@
 'use client';
 
 import React from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import {
+  LineChart,
+  CartesianGrid,
+  Label,
+  Legend,
+  Line,
+  PolarAngleAxis,
+  PolarGrid,
+  PolarRadiusAxis,
+  Radar,
+  RadarChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+
 
 import styles from './RechartsTest.module.css';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  }
-];
-
-export interface RechartsTestProps {
+export interface RechartsTestLineProps {
   prop?: string;
 }
 
-const RechartsTest = ({ prop = 'default value' }: RechartsTestProps) => {
+const RechartsTestLine = ({ prop = 'default value' }: RechartsTestLineProps) => {
   return (
-    <React.Fragment>
-      <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <Line type="monotone" strokeWidth={5} dataKey="uv" stroke="var(--color-accent-2)" />
-        <Line type="monotone" strokeWidth={5} dataKey="pv" stroke="var(--color-accent-3)" />
-        <Line type="monotone" strokeWidth={5} dataKey="amt" stroke="var(--color-accent-4)" />
+    <ResponsiveContainer width="100%" height={400} >
+      <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <Line type="monotone" strokeWidth={5} dataKey="Rating" stroke="var(--color-accent-2)" />
+        <Line type="monotone" strokeWidth={5} dataKey="THC" stroke="var(--color-accent-3)" />
+        <Line type="monotone" strokeWidth={5} dataKey="THCa" stroke="var(--color-accent-4)" />
+        <Line type="monotone" strokeWidth={5} dataKey="Δ9-THC" stroke="var(--color-accent-5)" />
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="Strain"   >
+          <Label value="Strains" offset={0} position="insideBottom" />
+        </XAxis>
         <YAxis />
+        <Legend />
         <Tooltip />
       </LineChart>
-
-    </React.Fragment>
+    </ResponsiveContainer>
   );
 }
 
-export { RechartsTest }
+export interface RechartsTestRadarProps {
+  data?: [];
+}
+
+const RechartsTestRadar = ({data}: RechartsTestRadarProps) => {
+  // console.log(['data', data])
+
+  return (
+    <ResponsiveContainer width="100%" height={600} >
+      <RadarChart
+        outerRadius={200}
+        width={400}
+        height={400}
+        data={data}
+        className={styles.RechartsTest}>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="Strain" />
+        <PolarRadiusAxis
+          angle={90}
+          domain={[0, 'dataMax']}
+          />
+        <Radar
+          name="THC"
+          dataKey="combinedTHC"
+          stroke="var(--color-accent-2-dark)"
+          fill="var(--color-accent-2-light)"
+          fillOpacity={0.6}
+          />
+        <Legend
+          align='center'
+          verticalAlign='top'
+          layout='vertical'
+          />
+        <Tooltip />
+      </RadarChart>
+    </ResponsiveContainer>
+  )
+}
+
+export {
+  RechartsTestLine,
+  RechartsTestRadar
+}
 
 
