dee570d7a4a726f9e9b7523e5c695087fed86ad1
Gwen Wendell <gwen@thecodingdesigner.com>
2025-02-17 13:40:33 -0500

    🛠️ app/experiments/page.tsx -> Imports new chart types

--- app/experiments/page.tsx
+++ app/experiments/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { DecoratedHeading } from "../components/DecoratedHeading";
 import { ParagraphWrapper } from "../components/ParagraphWrapper";
-import { RechartsTest } from "../components/experiments/components";
+import {
+  RechartsTestLine,
+  RechartsTestRadar
+} from "../components/experiments/components";
+
+import data from "../components/experiments/components/data/weed.json"
+
+const transformData = (data) => {
+  let transformedData =
+    data.filter((item) => item.Form === "Flower")
+
+  transformedData.forEach((item) => {
+    // console.log(['item', item])
+    item["combinedTHC"] = item.THC + item.THCVa + item.THCa + item['Δ9-THC']
+  })
+
+  return transformedData
+}
 
 export const metadata = {
   title: "Experiments",
@@ -13,9 +30,10 @@ const Page = () => {
       <DecoratedHeading background="pink">Experiments</DecoratedHeading>
 
       <ParagraphWrapper>
-        Recharts - FAILED
+        Recharts
       </ParagraphWrapper>
-      <RechartsTest />
+      <RechartsTestRadar data={transformData(data)} />
+      {/* <RechartsTestLine /> */}
 
     </React.Fragment>
   );
